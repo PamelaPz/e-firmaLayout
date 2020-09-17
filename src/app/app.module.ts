6d79e3440863730ca68fb922bdeec0d2810b00ac
PamelaPz <jpamelapz02@gmail.com>
2020-09-17 14:58:22 -0500

    sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
