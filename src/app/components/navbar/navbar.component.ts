5e742595580e42037072a010f2bd131f16eaced9
PamelaPz <jpamelapz02@gmail.com>
2020-09-17 14:58:11 -0500

    navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent implements OnInit {
+
+  public isMenuCollapsed = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
